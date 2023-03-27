35a38c562972dbba47872fd008cde36457f3fcc4
Samuel Hyrum <samuelhymp@gmail.com>
2023-03-27 12:32:14 -0300

    Requisito 7, Implemente a função filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -15,18 +15,9 @@ def get_unique_industries(path: str) -> List[str]:
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    data = jobs
+    lista = []
+    for item in data:
+        if item["industry"] == industry:
+            lista.append(item)
+    return lista
